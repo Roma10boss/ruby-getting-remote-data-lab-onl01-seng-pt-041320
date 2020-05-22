b62a8f0c1ab7459c4ce1cd12cf75bf9a3c905187
Romario Gustave <romagustave10@gmail.com>
2020-05-22 14:57:31 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,2 +1,16 @@
 # Write your code here
-class GetRequester 
+require 'net/http'
+require 'open-uri'
+require 'json'
+ 
+class GetRequester
+ 
+  URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+ 
+  def get_programs
+    uri = URI.parse(URL)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
+ 
+end
